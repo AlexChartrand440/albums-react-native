4912e6ff2af260165431bf8b513691b67db16256
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-15 17:20:00 +0100

    Refactor Add Header component

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,7 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // Make component
-const Header = () => (
-  <Text>Albums!</Text>;
-);
+const Header = () => <Text>Albums!</Text>;
 
 // Export component
 export default Header;
