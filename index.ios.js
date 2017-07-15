777fedda7d0631ebf7e6ae1b702899e02e8607f3
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-15 15:26:24 +0100

    Rendering initial component

--- index.ios.js
+++ index.ios.js
@@ -1,1 +1,15 @@
 // Index.ios.js - place code in here for ios
+
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create component
+const App = () => (
+  <Text>Some Text</Text>
+);
+
+// Render it to the device
+// Render application 'albums'
+// This must match up with the project name
+AppRegistry.registerComponent('albums', () => App);
